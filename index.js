4f18b755e06a7dc6b52620cce17b0a880c6af4f0
ryanBlewin <lewin.ry@gmail.com>
2020-08-08 12:25:39 +1000

    Adds cleaning functions to make the data recieved usable in the QLD online validation tool

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const testData = require('./test_data');
+const cleaner = require('./clean');
 const qldChecker = require('./qld_checker');
 
 async function getResult(testPerson) {
-  const result  = await qldChecker(testPerson)
+  const result  = await qldChecker(cleaner(testPerson))
   console.log(result)
 }
 
-getResult(testData.bluecard);
+// getResult(testData.passCase);
+getResult(testData.failCase);
